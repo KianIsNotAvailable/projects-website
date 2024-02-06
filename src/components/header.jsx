f9db4e5d5142257fb9f284028d3fb18a9da9d187
KianIsNotAvailable <kianxw@gmail.com>
2024-02-06 18:26:08 +0000

    cv

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import cv from '../assets/tech cv.pdf'
 import { Link } from 'react-scroll';
 import { Link as RouterLink } from 'react-router-dom';
 
@@ -11,7 +11,7 @@ export default function header() {
     <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center cursor-pointer">
       <a class="mr-5 hover:text-white"><RouterLink to='/'>Home</RouterLink></a>
       <a class="mr-5 hover:text-white"><Link to='experience' spy={true} smooth={true} offset={-70} duration={500}><RouterLink to='/'>Projects</RouterLink></Link></a>
-      <a class="mr-5 hover:text-white">PDF CV</a>
+      <a class="mr-5 hover:text-white" href={cv} target='_blank'>PDF CV</a>
     </nav>
   </div>
 </header>
