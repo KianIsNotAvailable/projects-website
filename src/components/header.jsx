04fdc73463cc6298bbfb2b839e17035b6caaacba
KianIsNotAvailable <kianxw@gmail.com>
2024-02-07 15:01:48 +0000

    pdf

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import cv from '../assets/tech cv.pdf'
+
 import { Link } from 'react-scroll';
 import { Link as RouterLink } from 'react-router-dom';
 
 export default function header() {
+  const pdfPath = process.env.PUBLIC_URL + '/tech cv.pdf';
   return (
     <div>
         <header class="text-gray-400 bg-gray-900 body-font">
@@ -11,7 +12,7 @@ export default function header() {
     <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center cursor-pointer">
       <a class="mr-5 hover:text-white"><RouterLink to='/'>Home</RouterLink></a>
       <a class="mr-5 hover:text-white"><Link to='experience' spy={true} smooth={true} offset={-70} duration={500}><RouterLink to='/'>Projects</RouterLink></Link></a>
-      <a class="mr-5 hover:text-white" href={cv} target='_blank'>PDF CV</a>
+      <a class="mr-5 hover:text-white" href={pdfPath} target='_blank'>PDF CV</a>
     </nav>
   </div>
 </header>
